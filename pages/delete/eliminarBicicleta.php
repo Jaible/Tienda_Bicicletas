2bd2907e47ef46b6b4fbb62620dea9b25e335222
sebasrodrip <106126186+sebasrodrip@users.noreply.github.com>
2023-04-27 18:39:21 -0600

    cambio menor

--- pages/delete/eliminarBicicleta.php
+++ pages/delete/eliminarBicicleta.php
@@ -1,7 +1,7 @@
 <?php
         include_once("../conexionDB.php");
         $conn = abrirConexion();
-		$nombre = $_GET["id"];
+		$id = $_GET["id"];
         $query = "DELETE FROM BICICLETAS WHERE ID_BICICLETAS = :id";
         $stid = oci_parse($conn, $query);
 		oci_bind_by_name($stid, ':id', $id);
